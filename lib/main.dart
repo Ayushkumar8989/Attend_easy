854c7ca66dafc659cd57e46a49a3c07eb83d644e
ayush <ayushkumar8709419452@gmail.com>
2024-09-13 23:13:27 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Ayush',
+              'Ayush kumar',
             ),
             Text(
               '$_counter',
