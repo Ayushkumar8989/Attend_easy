89411f15f895662c27862ebc6f0f2bad1514d901
ayush <ayushkumar8709419452@gmail.com>
2024-09-13 23:28:00 +0530

    decision

--- lib/faculty/decisionfac.dart
+++ lib/faculty/decisionfac.dart
@@ -1,5 +1,5 @@
-import 'package:attendance/faculty/loginfac.dart';
-import 'package:attendance/faculty/signinfac.dart';
+import 'package:attend_easy/faculty/loginfac.dart';
+import 'package:attend_easy/faculty/signinfac.dart';
 import 'package:flutter/material.dart';
 
 class DecisionFac extends StatefulWidget {
