d8a0736bc61a5a060ea3d610919f82aa3341a2b0
ayush <ayushkumar8709419452@gmail.com>
2024-09-24 22:02:11 +0530

    bottom

---
+++ lib/faculty/bottom_nav.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Bottom_Nav extends StatefulWidget {
+  const Bottom_Nav({super.key});
+
+  @override
+  State<Bottom_Nav> createState() => _Bottom_NavState();
+}
+
+class _Bottom_NavState extends State<Bottom_Nav> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
