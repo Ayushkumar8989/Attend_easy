8d93a7607733dd7198c34a189e073587d04a6ed5
ayush <ayushkumar8709419452@gmail.com>
2024-09-24 22:56:14 +0530

    changes

--- lib/faculty/bottom_nav.dart
+++ lib/faculty/bottom_nav.dart
@@ -1,4 +1,8 @@
+import 'package:attend_easy/faculty/history.dart';
+import 'package:attend_easy/faculty/home.dart';
+import 'package:attend_easy/faculty/profile.dart';
 import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 
 class Bottom_Nav extends StatefulWidget {
   const Bottom_Nav({super.key});
@@ -8,10 +12,43 @@ class Bottom_Nav extends StatefulWidget {
 }
 
 class _Bottom_NavState extends State<Bottom_Nav> {
+  final _controller = PersistentTabController(initialIndex: 0);
+
+  List<Widget> screens() {
+    return [
+      const Home(),
+      const History(),
+      const Profile(),
+    ];
+  }
+
+  List<PersistentBottomNavBarItem> navBarItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.home),
+        title: "Home",
+        activeColorPrimary: Colors.blue,
+        inactiveColorPrimary: Colors.yellow,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.history),
+        title: "History",
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.account_circle),
+        title: "Profile",
+      ),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
+    return PersistentTabView(
+      context,
+      screens: screens(),
+      items: navBarItems(),
+      controller: _controller,
+      navBarStyle: NavBarStyle.style1,
     );
   }
 }
