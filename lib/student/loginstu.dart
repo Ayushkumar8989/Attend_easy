34c52f1ee287540331e82a6e03b3df84e2d56832
Ayushkumar8989 <ayushkumar8709419452@gmail.com>
2024-10-03 00:31:55 +0530

    loginstu

--- lib/student/loginstu.dart
+++ lib/student/loginstu.dart
@@ -11,168 +11,172 @@ class LoginStu extends StatefulWidget {
 class _LoginState extends State<LoginStu> {
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          width: screenWidth * 0.9,
-          height: screenHeight * 0.8,
-          color: const Color.fromARGB(255, 249, 249, 249),
-          margin: const EdgeInsets.only(top: 70),
-          child: Column(
-            children: [
-              Container(
-                width: screenWidth * 0.7,
-                height: 50,
-                margin: const EdgeInsets.only(left: 45),
-                child: const Text(
-                  'Welcome back to AttendEasy!',
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
-                      fontFamily: 'DM Sans',
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-              Container(
-                width: screenWidth * 0.7,
-                height: 50,
-                margin: const EdgeInsets.only(left: 45),
-                child: const Text(
-                  'Log in to manage classes and track attendance seamlessly.',
-                  style: TextStyle(
-                      fontSize: 12,
-                      color: Colors.black,
-                      fontFamily: 'DM Sans',
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-              Container(
-                width: screenWidth * 0.7,
-                height: 50,
-                margin: const EdgeInsets.only(left: 45),
-                child: const Text(
-                  'Student ID',
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
-                      fontFamily: 'DM Sans',
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-              Container(
-                width: screenWidth * 0.7,
-                height: 50,
-                margin: const EdgeInsets.only(left: 45),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: ' Enter your id number',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                width: screenWidth * 0.7,
-                height: 50,
-                margin: const EdgeInsets.only(left: 45),
-                child: const Text(
-                  'Password',
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
-                      fontFamily: 'DM Sans',
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-              Container(
-                width: screenWidth * 0.7,
-                height: 50,
-                margin: const EdgeInsets.only(left: 45),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: ' Enter your password',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                width: screenWidth * 0.7,
-                margin: const EdgeInsets.only(left: 38),
-                child: Row(
+    return LayoutBuilder(
+      builder: (context, Constraints) {
+        bool isDesktop = Constraints.maxWidth > 600;
+        double screenWidth = Constraints.maxWidth;
+        double screenHeight = Constraints.maxHeight;
+        return Scaffold(
+          // appBar: AppBar(
+          //   title: Text('Welcome back to AttendEasy!'),
+          // ),
+          body: SingleChildScrollView(
+            child: Center(
+              child: Container(
+                width: screenWidth * 1.0,
+                height: screenHeight * (isDesktop ? 1.0 : 1.0),
+                color: Colors.white,
+                child: Column(
                   children: [
                     Container(
-                      child: const Text('Forgot Password?'),
+                      //width: screenWidth * 0.7,
+                      width: screenWidth * (isDesktop ? 0.6 : 0.85),
+                      height: 50,
+                      margin: const EdgeInsets.only(top: 70),
+                      child: Text(
+                        'Welcome back to AttendEasy!',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontFamily: 'DM Sans',
+                            fontWeight: FontWeight.bold),
+                      ),
                     ),
                     Container(
-                      margin: const EdgeInsets.only(right: 15),
-                      child: TextButton(
-                        onPressed: () {},
-                        child: const Text(
-                          'Reset it',
-                          style: TextStyle(
-                            color: Color(0xFF1C5B41),
+                      width: screenWidth * (isDesktop ? 0.6 : 0.85),
+                      height: 50,
+                      child: Text(
+                        'Log in to manage classes and track attendance seamlessly.',
+                        style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.black,
+                            fontFamily: 'DM Sans',
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Container(
+                      width: screenWidth * (isDesktop ? 0.6 : 0.85),
+                      height: 50,
+                      child: Text(
+                        'Stud ID',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontFamily: 'DM Sans',
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Container(
+                      width: screenWidth * (isDesktop ? 0.6 : 0.85),
+                      height: 50,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: ' Enter your number',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
                           ),
                         ),
                       ),
                     ),
-                  ],
-                ),
-              ),
-              Container(
-                width: screenWidth * 0.7,
-                margin: const EdgeInsets.only(left: 45),
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFF1DC99E),
-                    foregroundColor: Colors.white,
-                  ),
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(fontSize: 20, fontFamily: 'Inter'),
-                  ),
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(left: 45),
-                width: screenWidth * 0.7,
-                child: Row(
-                  children: [
                     Container(
-                      child: const Text('Dont have an account'),
+                      width: screenWidth * (isDesktop ? 0.6 : 0.85),
+                      height: 50,
+                      child: Text(
+                        'Password',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontFamily: 'DM Sans',
+                            fontWeight: FontWeight.bold),
+                      ),
                     ),
                     Container(
-                      margin: const EdgeInsets.only(right: 15),
-                      child: TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const SignInStu(),
+                      width: screenWidth * (isDesktop ? 0.6 : 0.85),
+                      height: 50,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: ' Enter your password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: screenWidth * (isDesktop ? 0.6 : 0.85),
+                      height: 50,
+                      child: Row(
+                        children: [
+                          Container(
+                            child: const Text('Forgot Password?'),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(right: 15),
+                            child: TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                'Reset it',
+                                style: TextStyle(
+                                  color: Color(0xFF1C5B41),
+                                ),
+                              ),
                             ),
-                          );
-                        },
-                        child: const Text(
-                          'Sign Up',
-                          style: TextStyle(
-                            color: Color(0xFF1DC99E),
                           ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: screenWidth * (isDesktop ? 0.6 : 0.85),
+                      height: 50,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF1DC99E),
+                          foregroundColor: Colors.white,
                         ),
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(fontSize: 20, fontFamily: 'Inter'),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: screenWidth * (isDesktop ? 0.6 : 0.85),
+                      height: 50,
+                      child: Row(
+                        children: [
+                          Container(
+                            child: const Text('Dont have an account'),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(right: 15),
+                            child: TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const SignInStu(),
+                                  ),
+                                );
+                              },
+                              child: const Text(
+                                'Sign Up',
+                                style: TextStyle(
+                                  color: Color(0xFF1DC99E),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
                 ),
               ),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
