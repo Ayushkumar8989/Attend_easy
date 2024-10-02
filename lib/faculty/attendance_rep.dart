ac63c1846bce45556ab36059254cab3a6a97d1da
Ayushkumar8989 <ayushkumar8709419452@gmail.com>
2024-10-03 00:14:30 +0530

    attendance

---
+++ lib/faculty/attendance_rep.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class AttendanceRep extends StatefulWidget {
+  const AttendanceRep({super.key});
+
+  @override
+  State<AttendanceRep> createState() => _AttendanceRepState();
+}
+
+class _AttendanceRepState extends State<AttendanceRep> {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, Constraints) {
+      bool isDesktop = Constraints.maxWidth > 600;
+      double screenWidth = Constraints.maxWidth;
+      double screenHeight = Constraints.maxHeight;
+      return Scaffold(
+        body: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              width: screenWidth * 1.0,
+              height: screenHeight * 1.0,
+              color: Colors.white,
+              child: Column(
+                children: [
+                  Container(
+                    width: screenWidth * (isDesktop ? 0.6 : 0.85),
+                    child: Center(
+                      child: Text(
+                        'Attendance Report',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontFamily: 'DM Sans',
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 1.0,
+                    height: 5,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+    });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:attend_easy/faculty/attendance_rep.dart';
 import 'package:attend_easy/faculty/new_session.dart';
 import 'package:attend_easy/welcome.dart';
 import 'package:flutter/material.dart';
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Welcome(),
+      home: AttendanceRep(),
     );
   }
 }
