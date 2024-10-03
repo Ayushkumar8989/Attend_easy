1b2fd2ce0357ff2f202c53b36ac97e410572699a
Shivam123-code <shivambabu179@gmail.com>
2024-10-03 20:35:58 +0530

    splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:attend_easy/faculty/new_session.dart';
+import 'package:attend_easy/splash_screen.dart';
 import 'package:attend_easy/welcome.dart';
 import 'package:flutter/material.dart';
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Welcome(),
+      home: splash_screen(),
     );
   }
 }
